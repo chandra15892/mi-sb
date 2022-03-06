f554e7f7f1926a1a118b80af5485a1208ef79561
chandrashekar <chandrashekar15892@gmail.com>
2022-03-06 10:19:47 +0530

    base code changes

---
+++ src/main/java/com/example/gl/azureblob/config/AzureStorageBlobClientConfig.java
@@ -0,0 +1,41 @@
+package com.example.gl.azureblob.config;
+
+
+import com.azure.identity.ManagedIdentityCredential;
+import com.azure.identity.ManagedIdentityCredentialBuilder;
+import com.azure.storage.blob.*;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AzureStorageBlobClientConfig {
+
+    @Value("${blob.connection-string}")
+    String connectionString;
+
+    @Value("${blob.container-name}")
+    String containerName;
+    
+    @Value("${blob.client-id}")
+    String clientId;
+    
+    @Value("${blob.storage-account}")
+    String storageAccount;
+
+    @Bean
+    public BlobClientBuilder getClient() {
+    	
+//    	  String endpoint = "https://<storageAccount>.blob.core.windows.net";
+    	  String endpoint = "https://"+storageAccount+".blob.core.windows.net";
+
+    	ManagedIdentityCredential managedIdentityCredential = new ManagedIdentityCredentialBuilder().clientId(clientId).build();
+    	
+        BlobClientBuilder client = new BlobClientBuilder();
+        client.endpoint(endpoint);
+        client.credential(managedIdentityCredential);
+//        client.connectionString(connectionString);
+        client.containerName(containerName);
+        return client;
+    }
+}
